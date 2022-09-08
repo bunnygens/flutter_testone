bb99e0177ce8b86faefd20853fdab1cb1d34d9a4
Bunny Gens <bunnygens1@gmail.com>
2022-09-08 10:14:50 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_testone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_testone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.green[700],
+        title: const Text("Tesla Corp"),
+      ),
+      body: const Center(
+        child: Text('Home page'),
+      ),
+    );
+  }
+}
